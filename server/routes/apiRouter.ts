145e9657eab5d8138433ef92e8f1c31a1041bcd1
HelpDesk <mbarksdale061@gmail.com>
2025-02-26 21:16:50 -0600

    build: added boilerplate for server, router, and added some folders to be populated later

---
+++ server/routes/apiRouter.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const apiRouter = express.Router();
+
+apiRouter.use('/vector');
+apiRouter.use('/books');
+apiRouter.use('/scenes');
+apiRouter.use('/image');
+
+export default apiRouter;
