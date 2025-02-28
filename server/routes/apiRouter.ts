9650e3387db68b1ee06d0ce8c2d9c40e456e514e
HelpDesk <mbarksdale061@gmail.com>
2025-02-27 20:21:34 -0600

    fix: the server will listen now on server 3000.

--- server/routes/apiRouter.ts
+++ server/routes/apiRouter.ts
@@ -2,9 +2,9 @@ import express from 'express';
 
 const apiRouter = express.Router();
 
-apiRouter.use('/vector');
-apiRouter.use('/books');
-apiRouter.use('/scenes');
-apiRouter.use('/image');
+// apiRouter.use('/vector');
+// apiRouter.use('/books');
+// apiRouter.use('/scenes');
+// apiRouter.use('/image');
 
-export default apiRouter;
+export { apiRouter };
--- server/routes/scenesRouter.ts
+++ server/routes/scenesRouter.ts
@@ -2,10 +2,13 @@ import express from 'express';
 
 const scenesRouter = express.Router();
 
+const controller = {
+    middleware: () =>{ console.log('im a fake function >:)')}
+}
 //example route
 scenesRouter.post('/get', controller.middleware, (req, res) => {
     res.status(200).json(res.locals.chores);
-})
+});
 
 
 export { scenesRouter };
