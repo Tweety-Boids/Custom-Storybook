be6661c043a1fa1eba0238226b410741870c2be3
Alicia <130313978+abrooks11@users.noreply.github.com>
2025-02-27 18:32:11 -0600

    feature (App) initial component structure with imports. minimal styling

---
+++ client/src/components/books/Book.tsx
@@ -0,0 +1,13 @@
+const Book = () => {
+  return (
+    <>
+      <div className="book-wrapper">
+        <h1>--BOOK--</h1>
+        <div className='book-img-wrapper'>**img**</div>
+        <h1>Title</h1>
+        </div>
+    </>
+  );
+};
+
+export default Book;
---
+++ client/src/components/characters/CharacterList.tsx
@@ -0,0 +1,10 @@
+const CharacterList = () => {
+  return (
+    <>
+      <h1>--CHARACTER LIST--</h1>
+      <div className="character-list-wrapper"></div>
+    </>
+  );
+};
+
+export default CharacterList;
