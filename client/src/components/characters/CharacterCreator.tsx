f798f1d14726cce998f4d903211b780120ebcd16
Alicia <130313978+abrooks11@users.noreply.github.com>
2025-03-01 16:47:20 -0600

    feature (dropdown and textinput) added props to make more reusable

--- client/src/components/characters/CharacterCreator.tsx
+++ client/src/components/characters/CharacterCreator.tsx
@@ -2,15 +2,31 @@ import Dropdown from '../ui/Dropdown'
 import TextInput from '../ui/TextInput'
 
 const CharacterCreator = () => {
+// define DropDown options 
+  const hairColors = {
+    black: 'Black', 
+    brown:'Brown', 
+    blonde:'Blonde', 
+    red:'Red', 
+    grey:'Grey', 
+  }
+
+  const eyeColors = {
+    blue: 'Blue', 
+    black: 'Black', 
+    brown:'Brown', 
+    Green:'Green', 
+  }
+
     return (
       <>
         <h1>--CHARACTER CREATOR--</h1>
         <div className="character-creator-wrapper">
-          <TextInput />
-          <TextInput />
-          <Dropdown />
-          <Dropdown />
-          <Dropdown />
+          <TextInput label='Name'/>
+          <TextInput label="Catch Phrase"/>
+          <TextInput label="Special Talent"/>
+          <Dropdown label="Hair Color" optionList={hairColors}/>
+          <Dropdown label="Eye Color" optionList={eyeColors}/>
         </div>
       </>
     );
