1bc9f19bf848c51968e2cec0615b25d7ea8c6247
HelpDesk <mbarksdale061@gmail.com>
2025-03-01 13:36:51 -0600

    build: created controllers for openaiController, pineconeController created upsert middle ware for vector database infrastructure for ai querying and placeholder data created

--- types/types.ts
+++ types/types.ts
@@ -2,4 +2,13 @@ export type ServerError = {
     log: string;
     status: number;
     message: { err: string };
-  };
+};
+
+export type StoryMetadata = {
+  id: string;
+  title: string;
+  story: string;
+  characters: string[];
+  genre: string;
+  author: string;
+};
