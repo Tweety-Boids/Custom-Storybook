60d75d01ebe0124303dc6a8491c019be6eedbbc8
Alicia <130313978+abrooks11@users.noreply.github.com>
2025-03-01 15:20:19 -0600

    feature (storyPanel) added ability to toggle display of story panel via button click

--- client/src/components/layout/StoryPanel.tsx
+++ client/src/components/layout/StoryPanel.tsx
@@ -1,16 +1,31 @@
+// import state management components
+import useGlobalContext from "../../hooks/useGlobalContext";
+import { ActionTypes } from "../../context/GlobalContext";
+
+// import state rendering components
 import BookCreator from "../books/BookCreator";
 import CharacterCreator from "../characters/CharacterCreator";
 import CharacterList from "../characters/CharacterList";
 
 const StoryPanel = () => {
+  const { dispatch } = useGlobalContext();
+
+  const toggleSidePanel = () => {
+    console.log("toggleSidePanel button clicked");
+    dispatch({
+      type: ActionTypes.TOGGLE_STORY_PANEL,
+      payload: true
+    })
+  };
+
   return (
-    <div className='story-panel-wrapper'>
+    <div className="story-panel-wrapper">
       <h1>--STORY PANEL--</h1>
       <BookCreator />
       <CharacterCreator />
       <CharacterList />
     </div>
-  )
-}
+  );
+};
 
-export default StoryPanel
+export default StoryPanel;
