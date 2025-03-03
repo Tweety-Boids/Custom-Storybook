45192c2f57eb45144712a0ed0c28aa51c1f99920
Alicia <130313978+abrooks11@users.noreply.github.com>
2025-03-02 18:13:05 -0600

    testing (package.json) installed testing dependencies: vitest, jsdom, React Testing Library; modified client tsconfig and vite.config to integrate testing;

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
-})
+  test: {
+    globals: true, // Enables global test functions like describe, it, expect
+    environment: "jsdom", // Use 'node' for backend tests
+    coverage: {
+      reporter: ["text", "json", "html"], // Optional: Enable coverage reporting
+    },
+  },
+
+});
